abfde176bd5bf32ba1e16dc1bb927069d1870ad0
adamfariello <adam.fariello@rutgers.edu>
2022-04-14 03:37:29 -0400

    >? ?'

--- src/FXMLcontrollers/userPage_fileRename.java
+++ src/FXMLcontrollers/userPage_fileRename.java
@@ -11,12 +11,13 @@ import ourFilesTM.Album;
 public class userPage_fileRename {
 	@FXML private TextField TextField;
 	private Album currDir;
-	public void init(Album currDir) {
+	private Object object;
+	public void init(Album currDir, Object object) {
 		this.currDir = currDir;
+		this.object = object;
 	}
 	@FXML private void create(ActionEvent event) {
-		Album album = new Album(TextField.getText());
-		currDir.addFile(album);
+		currDir.getFile(object);
 		exit(event);
 	}
 	@FXML private void exit(ActionEvent event) {
